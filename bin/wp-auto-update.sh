cc07bc31a0df02076c3468c71680d30df7f15fd3
Hugo Pinheiro <hpinheiro@echidna.ca>
2018-11-19 14:18:45 -0500

    update ci

--- bin/wp-auto-update.sh
+++ bin/wp-auto-update.sh
@@ -49,7 +49,7 @@ terminus env:wake -n $SITE_UUID.$TERMINUS_ENV
 # check for Drupal module updates
 echo -e "\nChecking for Drupal module updates on the ${TERMINUS_ENV} multidev..."
 terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus
-PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus --dry-run --all --format=summary)"
+PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus --format=list | grep -v ok)"
 
 if [[ ${PLUGIN_UPDATES} == "No module updates available." ]]
 then
